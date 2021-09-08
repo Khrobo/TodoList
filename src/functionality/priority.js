bfffd44f4061b781cf08047bfcceec1249ba64fe
Khrononian <71100908+Khrononian@users.noreply.github.com>
2021-09-07 23:08:59 -0400

    Added deletion for arrays

--- src/functionality/priority.js
+++ src/functionality/priority.js
@@ -1,3 +1,5 @@
+import { projectArray } from "./todolist";
+
 const circles = document.querySelectorAll(".fa-circle");
 const times = document.querySelectorAll(".fa-times");
 
@@ -5,7 +7,22 @@ const circleShader = event => {
     event.target.classList.toggle("green")
 }
 const removeTask = event => {
-    event.target.parentElement.parentElement.remove()
+    event.target.parentElement.parentElement.remove();
+    const findElement = event.target.parentElement.parentElement.querySelector(".start").querySelector(".task-name").innerText;
+
+    // DELETING ITEMS THROUGH ARRAYS WORKS NOW
+    
+    for (let i = 0; i < projectArray.length; i++) {
+        console.log(projectArray)
+        if (projectArray[i].task == findElement) {
+            const findIndex = projectArray.indexOf(projectArray[i])
+            console.log(projectArray[i], findIndex);
+            projectArray.splice(findIndex, 1);
+            console.log(projectArray);
+        }
+    }
+    
+    
 }
 
 circles.forEach(e => e.addEventListener("click", circleShader)) // ADD SAVES
