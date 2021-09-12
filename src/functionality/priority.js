22e44fcf211f208e8f3a49094a5718b002b2706f
Khrononian <71100908+Khrononian@users.noreply.github.com>
2021-09-11 22:20:59 -0400

    Added functionality for completions

--- src/functionality/priority.js
+++ src/functionality/priority.js
@@ -4,29 +4,45 @@ const circles = document.querySelectorAll(".fa-circle");
 const times = document.querySelectorAll(".fa-times");
 
 const circleShader = event => {
-    event.target.classList.toggle("green")
+    const listBtn = document.querySelectorAll(".list-btn");
+    const findElement = event.target.parentElement.parentElement.querySelector(".start").querySelector(".task-name").innerText;
+    
+    event.target.classList.toggle("green");
+    for (let k = 0; k < projectArray.length; k++) {
+        for (let j = 0; j < listBtn.length; j++) {
+            if (listBtn[j].style.background == "grey" && 
+            listBtn[j].value == projectArray[k].title && 
+            findElement == projectArray[k].task && !(projectArray[k].check)) {
+                projectArray[k].check = true;
+                console.log(projectArray);
+                return;
+            } else if (listBtn[j].style.background == "grey" && 
+            listBtn[j].value == projectArray[k].title && 
+            findElement == projectArray[k].task && projectArray[k].check == true) {
+                projectArray[k].check = false;
+                console.log("False check", projectArray)
+                return;
+            }
+        }
+    }
 }
 const removeTask = event => {
-    event.target.parentElement.parentElement.remove();
     const findElement = event.target.parentElement.parentElement.querySelector(".start").querySelector(".task-name").innerText;
-
-    // DELETING ITEMS THROUGH ARRAYS WORKS NOW
     
+    event.target.parentElement.parentElement.remove();
     for (let i = 0; i < projectArray.length; i++) {
         console.log(projectArray)
         if (projectArray[i].task == findElement) {
-            const findIndex = projectArray.indexOf(projectArray[i])
+            const findIndex = projectArray.indexOf(projectArray[i]);
             console.log(projectArray[i], findIndex);
             projectArray.splice(findIndex, 1);
-            console.log(projectArray);
+            console.log("Spliced array", projectArray);
+            return;
         }
-    }
-    
-    
+    } 
 }
 
-circles.forEach(e => e.addEventListener("click", circleShader)) // ADD SAVES
-times.forEach(e => e.addEventListener("click", removeTask)) // ADD SAVES
-
+circles.forEach(e => e.addEventListener("click", circleShader)) 
+times.forEach(e => e.addEventListener("click", removeTask)) 
 
 export { removeTask, circleShader, circles, times }
