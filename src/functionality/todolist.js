11d3cc74893cdb72d775ce0265b9a55fe826f3e7
Khrononian <71100908+Khrononian@users.noreply.github.com>
2021-09-11 22:23:31 -0400

    Removed unnecessary code

--- src/functionality/todolist.js
+++ src/functionality/todolist.js
@@ -7,24 +7,6 @@ class TodoProjects {
     }
 }
 
-class MiscTodo {
-    constructor(title, task, date, check) {
-        this.title = title;
-        this.task = task;
-        this.date = date;
-        this.check = check;
-    }
-}
-
-class TodoTasks {
-    constructor(task, date) {
-        this.task = task;
-        this.date = date;
-    }
-}
-
 const projectArray = [];
-const taskArray = [];
-const miscArray = [];
 
-export { TodoProjects, TodoTasks, taskArray, projectArray, miscArray, MiscTodo }
+export { TodoProjects, projectArray }
